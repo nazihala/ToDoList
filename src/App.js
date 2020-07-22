c727a24a237c876712d78081a8cfc447f40219a4
nazihala <naziha.lahrach@gmail.com>
2020-07-22 11:18:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  const [task, SetTask]=useState("")
+  const [mytask, SetMytask]=useState("")
+  const [tasks, SetTasks]=useState([])
+ 
+  const handleChange=(e)=>{
+    e.preventDefault();
+    SetMytask(e.target.value)
+  }
+  const addTasks=(e)=>{
+    e.preventDefault();
+    SetTasks([...tasks,{name:mytask}]);
+    SetMytask("")
+  };
+ 
 
   
   return (
     <div className="App">
-        <input placeholder="Write your task" value={task} onChange={(e)=>SetTask(e.target.task)}></input>
-        <button type="submit">Add task</button>
-    
+      <form id="to-do-form" onSubmit={addTasks}>
+        <input placeholder="Enter text" type="text" name="task" value={mytask} onChange={handleChange}></input>
+        <button id="add-button" type="submit">Add task</button>
+      </form>
+      <ul className="unlist">
+        {tasks.map((task)=>(
+          <li className="list">{task.name}
+          <button id="delete-button"> X </button>
+        </li>
+        ))}
+        
+      </ul>
     </div>
   );
 }
